f4dfd9729a5d463d9731bd92861f9ac5d824ee4d
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2026-09-22 19:14:44 -0500

    Resolve #33, add flush to TuiLog

--- source/TuiLog.h
+++ source/TuiLog.h
@@ -1,5 +1,5 @@
 #ifndef TuiLog
-#define TuiLog(fmt__, ...) printf(fmt__"\n", ##__VA_ARGS__)
+#define TuiLog(fmt__, ...) do { printf(fmt__"\n", ##__VA_ARGS__); fflush(stdout); } while(0)
 #define TuiWarn(fmt__, ...) TuiLog("warning:" fmt__, ##__VA_ARGS__)
 #define TuiError(fmt__, ...) TuiLog("error:" fmt__, ##__VA_ARGS__)
 #endif
